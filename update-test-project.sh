ab4c20238f75123a5578e5230bc9e4a39da5f344
Josh Anthony <janthony@brokerbin.com>
2018-04-30 11:10:16 -0500

    changed the update script to refresh the DB instead of freshing

--- update-test-project.sh
+++ update-test-project.sh
@@ -3,6 +3,6 @@ composer update brokerexchange/showcase
 composer dump-autoload
 rm -rf ./resources/views/vendor/showcase # Re-publishing the assets does not update when the directories already exist, for some reason, so this is a hacky way to force updates.
 php artisan vendor:publish --tag=showcase
-php artisan migrate:fresh
+php artisan migrate:refresh
 php artisan db:seed --class=ShowcaseDatabaseSeeder
 echo "Test project updated!"

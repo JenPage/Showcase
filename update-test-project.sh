2b8a1c0a1e325a189c68b2cfcbe18e2b66a109cb
Josh Anthony <janthony@brokerbin.com>
2018-05-02 15:49:59 -0500

    fixed update script to actually check the migrations flag

--- update-test-project.sh
+++ update-test-project.sh
@@ -1,19 +1,18 @@
-while [ "${1:-}" != "" ]; do
-    case "$1" in
-        "-n" | "--nomigrations")
-            migrations=false
-            ;;
-        *)
-            migrations=true
-    esac
-    shift
-done
+case "$1" in
+    "-n" | "--nomigrations")
+        migrations=false
+        echo "Migrations will NOT run."
+        ;;
+    *)
+        migrations=true
+        echo "Migrations will run."
+esac
 
 cd test-project
 composer update brokerexchange/showcase
 composer dump-autoload
 php artisan vendor:publish --tag=showcase --force
-if [ migrations == true ]; then 
+if [ $migrations = true ]; then 
     php artisan migrate:refresh --seed
     php artisan db:seed --class=ShowcaseDatabaseSeeder
 fi

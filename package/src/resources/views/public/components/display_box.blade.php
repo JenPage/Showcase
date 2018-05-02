a981fa38e29b26d5f4d6e4dd6e1b74fa67907b04
Josh Anthony <janthony@brokerbin.com>
2018-05-02 12:06:03 -0500

    various style changes

--- package/src/resources/views/public/components/display_box.blade.php
+++ package/src/resources/views/public/components/display_box.blade.php
@@ -1,7 +1,7 @@
 <div class="showcase-display showcase-box">
     @foreach($display->trophies as $trophy)
     <div class="showcase-trophy">
-        <div class="showcase-trophy-image {{$display->imageOnly ?: " showcase-trophy-image-only "}}">
+        <div class="showcase-trophy-image">
             <img src="{{$trophy->image_url}}" alt="{{$trophy->alt}}">
         </div>
         <div class="showcase-trophy-title">

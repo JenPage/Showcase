095deda9963ec2b75871a54d28806def8f714ca2
Josh Anthony <janthony@brokerbin.com>
2018-05-08 08:18:33 -0500

    modified Create buttons in header

--- package/src/resources/views/app/includes/_header.blade.php
+++ package/src/resources/views/app/includes/_header.blade.php
@@ -5,7 +5,7 @@
             <li tabindex="-1" class="showcase-nav-item">
                 <a href="{{route('displays.index')}}">Displays</a>
                 <ul class="showcase-nav-container">
-                    <li class="showcase-nav-item"><a href="{{route('displays.create')}}">Create</a></li>
+                    <li class="showcase-nav-item"><a href="{{route('displays.create')}}">Create Display</a></li>
                     @foreach($displays as $display)
                     <li class="showcase-nav-item"><a href="{{route('displays.show', ['display' => $display])}}">{{$display->name}}</a></li>
                     @endforeach
@@ -16,7 +16,7 @@
             <li tabindex="-1" class="showcase-nav-item">
                 <a href="{{route('trophies.index')}}">Trophies</a>
                 <ul class="showcase-nav-container">
-                    <li class="showcase-nav-item"><a href="{{route('trophies.create')}}">Create</a></li>
+                    <li class="showcase-nav-item"><a href="{{route('trophies.create')}}">Create Trophy</a></li>
                     @foreach($trophies as $trophy)
                     <li class="showcase-nav-item"><a href="{{route('trophies.show', ['trophy' => $trophy])}}">{{$trophy->name}}</a></li>
                     @endforeach

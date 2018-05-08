8e8e363ab9df1d81bec06c608ef374886813f17e
Josh Anthony <janthony@brokerbin.com>
2018-05-08 09:25:44 -0500

    spruced up edit forms

--- package/src/resources/views/app/display/edit.blade.php
+++ package/src/resources/views/app/display/edit.blade.php
@@ -1,29 +1,30 @@
 @extends('showcase::app.layouts.app') 
 @section('title', 'Edit Display') 
 @section('content')
-<main class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <form action="{{route('displays.update', compact('display'))}}" method="POST">
-                {{method_field('PUT')}} {{csrf_field()}}
-                <div class="form-group">
-                    <label for="name">Display Name</label>
-                    <input class="form-control" type="text" name="name" value="{{$display->name}}">
-                </div>
-                <div class="form-group">
-                    <label for="component_view">Component View</label>
-                    <input class="form-control" type="text" name="component_view" value="{{$display->component_view}}">
-                </div>
-                <div class="form-group">
-                    <label for="default_trophy_component_view">Default Trophy Component View</label>
-                    <input class="form-control" type="text" name="default_trophy_component_view" value="{{$display->default_trophy_component_view}}">
-                    <label for="force_trophy_default">Force Default</label>
-                <input type="checkbox" name="force_trophy_default" value="yes" {{$display->force_trophy_default ? 'checked' : ''}}>
-                </div>
-                <button class="btn btn-success" type="submit">Submit</button>
-            </form>
+<main class="col-md-4 showcase-display-main">
+    <h1>Edit Display</h1>
+    <form action="{{route('displays.update', compact('display'))}}" method="POST">
+        {{method_field('PUT')}} {{csrf_field()}}
+        <div class="form-group">
+            <label for="name">Display Name</label>
+            <input class="form-control" type="text" name="name" value="{{$display->name}}">
         </div>
-    </div>
+        <div class="form-group">
+            <label for="component_view">Component View</label>
+            <input class="form-control" type="text" name="component_view" value="{{$display->component_view}}">
+        </div>
+        <div class="form-group">
+            <label for="default_trophy_component_view">Default Trophy Component View</label>
+            <input class="form-control" type="text" name="default_trophy_component_view" value="{{$display->default_trophy_component_view}}">
+        </div>
+        <div class="form-group form-check">
+            <input class="form-check-input" type="checkbox" name="force_trophy_default" value="yes" {{$display->force_trophy_default ? 'checked' : ''}}>
+            <label class="form-check-label" for="force_trophy_default">Force Default Trophy View</label>
+        </div>
+        <button class="btn btn-success btn-block" type="submit">Update</button>
+    </form>
 </main>
+
+
 
 @stop
--- package/src/resources/views/app/trophy/edit.blade.php
+++ package/src/resources/views/app/trophy/edit.blade.php
@@ -1,39 +1,39 @@
 @extends('showcase::app.layouts.app') 
 @section('title', 'Edit Trophy') 
 @section('content')
-<main class="col-md-6 col-md-offset-2">
+<main class="col-md-4 showcase-trophy-main">
     <h1>Edit Trophy</h1>
     <form action="{{route('trophies.update', compact('display', 'trophy'))}}" method="post">
         {{csrf_field()}} {{method_field('PUT')}}
         <div class="form-group">
             <label for="name">Component View</label>
-            <input type="text" name="component_view" value="{{$trophy->component_view}}">
+            <input class="form-control" type="text" name="component_view" value="{{$trophy->component_view}}">
         </div>
         <div class="form-group">
             <label for="name">Trophy Name</label>
-            <input type="text" name="name" value="{{$trophy->name}}">
+            <input class="form-control" type="text" name="name" value="{{$trophy->name}}">
         </div>
         <div class="form-group">
             <label for="link">Link</label>
-            <input type="text" name="link" value="{{$trophy->link}}">
+            <input class="form-control" type="text" name="link" value="{{$trophy->link}}">
         </div>
         <div class="form-group">
             <label for="image_url">Image URL</label>
-            <input type="text" name="image_url" value="{{$trophy->image_url}}">
+            <input class="form-control" type="text" name="image_url" value="{{$trophy->image_url}}">
         </div>
         <div class="form-group">
             <label for="description">Short Description</label>
-            <textarea name="description" id="" cols="30" rows="10">{{$trophy->description}}</textarea>
+            <input type="text" class="form-control" name="description" value={{$trophy->description}}>
         </div>
         <div class="form-group">
                 <label for="displays[]">Displays</label>
-                <select name="displays[]" id="" multiple="">
+                <select class="form-control" name="displays[]" id="" multiple="">
                     @foreach($displays as $display)
                     <option value="{{$display->id}}">{{$display->name}}</option>
                     @endforeach
                 </select>
             </div>
-        <button class="btn btn-success" type="submit">Edit Trophy</button>
+        <button class="btn btn-success btn-block" type="submit">Update</button>
     </form>
 </main>
 

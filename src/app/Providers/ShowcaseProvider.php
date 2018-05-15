bcf54609121a425a42ce969138beb247359d04fc
Josh Anthony <janthony@brokerbin.com>
2018-05-15 10:30:54 -0500

    forgot to include the validator

--- src/app/Providers/ShowcaseProvider.php
+++ src/app/Providers/ShowcaseProvider.php
@@ -3,6 +3,7 @@
 namespace Showcase\App\Providers;
 
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 

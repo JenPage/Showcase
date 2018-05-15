7eaf3f3e2a98af9f9fab520135f55c9481eb05f9
Josh Anthony <janthony@brokerbin.com>
2018-05-15 11:44:08 -0500

    updated custom validation messages to use package lang

--- src/resources/lang/en/validation.php
+++ src/resources/lang/en/validation.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'display_exists' => 'The display :attribute component file does not exist.',
-    'trophy_exists' => 'The trophy :attribute component file does not exist.',
+    'display_exists' => 'The display component file ":value.blade.php" does not exist.',
+    'trophy_exists' => 'The trophy component file ":value.blade.php" does not exist.',
 ];
